89f2e1810fb93db7febe39c253378b5ad470e594
Artem Shcherbakov <Artem2431@gmail.com>
2017-12-04 10:02:03 +0200

    temp navbar solution

--- app/js/main.js
+++ app/js/main.js
@@ -152,6 +152,51 @@
             }
         });
     },
+    navbar: function() {
+      var cbpHorizontalMenu = (function() {
+
+        var $listItems = $( '.nav-list > ul > li' ),
+          $menuItems = $listItems.children( 'a' ),
+          $body = $( 'body' ),
+          current = -1;
+          
+        function init() {
+          $menuItems.on( 'click', open );
+          $listItems.on( 'click', function( event ) { event.stopPropagation(); } );
+        }
+
+        function open( event ) {
+
+          if( current !== -1 ) {
+            $listItems.eq( current ).removeClass( 'cbp-hropen' );
+          }
+
+          var $item = $( event.currentTarget ).parent( 'li' ),
+            idx = $item.index();
+
+          if( current === idx ) {
+            $item.removeClass( 'cbp-hropen' );
+            current = -1;
+          }
+          else {
+            $item.addClass( 'cbp-hropen' );
+            current = idx;
+            $body.off( 'click' ).on( 'click', close );
+          }
+
+          return false;
+
+        }
+
+        function close( event ) {
+          $listItems.eq( current ).removeClass( 'cbp-hropen' );
+          current = -1;
+        }
+
+        return { init : init };
+
+      })();
+    }
 
 
   }
@@ -160,6 +205,7 @@
   init.custom();
   init.adventuresCustomSlider();
   init.swiper();
+  init.navbar();
   $(window).resize(function(){
       init.adventuresCustomSlider();
       init.swiper();
